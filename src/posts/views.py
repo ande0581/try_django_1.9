42e537f28c8257c9ca6cd790dcecf2e220c668cb
janderson <jeffrey.d.anderson@gmail.com>
2016-11-24 10:15:35 -0600

    added pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .models import Post
@@ -30,7 +31,18 @@ def post_detail(request, pk):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all().order_by('-timestamp')  # sorts by timestamp, newest first
+    paginator = Paginator(queryset_list, 5)  # Show 5 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {'title': 'list',
                'object_list': queryset}
     return render(request, 'post_list.html', context)
