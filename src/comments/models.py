ef1e96891de09e0267d4e102fbf9e9af411698d3
janderson <jeffrey.d.anderson@gmail.com>
2016-12-06 20:19:14 -0600

    added a new app for comments. Right now setup for using a foreignkey against posts. Will be exploring changing this to a generic foreignkey instead

---
+++ src/comments/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+from posts.models import Post
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+    post = models.ForeignKey(Post)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
