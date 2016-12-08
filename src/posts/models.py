697a25a8d10b8b330554940bcb69ef522e7737b0
janderson <jeffrey.d.anderson@gmail.com>
2016-12-07 18:14:27 -0600

    Create post comments

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,10 +1,12 @@
-from django.db import models
-from django.core.urlresolvers import reverse
-from django.db.models.signals import pre_save
-from django.utils.text import slugify
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.safestring import mark_safe
+from django.utils.text import slugify
+
 from markdown_deux import markdown
 from comments.models import Comment
 
@@ -89,6 +91,13 @@ class Post(models.Model):
         instance = self
         qs = Comment.objects.filter_by_instance(instance)
         return qs
+
+    @property
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
+
 
 # This is a recursive function
 def create_slug(instance, new_slug=None):
