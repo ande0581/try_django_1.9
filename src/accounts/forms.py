99859b19fd98a06b4f0567426eb3c8171aae94b6
ande0581 <jeffrey.d.anderson@gmail.com>
2016-12-27 19:56:57 -0600

    add more logic to login/logout forms and views

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.contrib.auth import authenticate, get_user_model, login, logout
+
+User = get_user_model()
 
 
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)  # this checks if the user exists
+
+            if not user:
+                raise forms.ValidationError("This user does not exist")
+            if not user.check_password(password):
+                raise forms.ValidationError("Incorrect Password")
+            if not user.is_active:
+                raise forms.ValidationError("This user is no longer active")
+            return super(UserLoginForm, self).clean(*args, **kwargs)
+
